88c1c99c59bf5d687dc8fd96e374fe2922c3bea5
khoriwill <your-email@example.com>
2026-04-28 19:21:31 -0400

    Clean deployment - remote state verified

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "securescale-terraform-state-485141928563"
+    key            = "securescale/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "securescale-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
